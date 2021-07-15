fbf315a69dc933a0b40c3b492f85da23fcce2d49
sujit2901 <87220723+sujit2901@users.noreply.github.com>
2021-07-15 18:27:03 +0530

    Add files via upload

---
+++ Pratik Restaurant website/index.php
@@ -0,0 +1,234 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="cache-control" content="max-age=0" />
+    <meta http-equiv="cache-control" content="no-cache" />
+    <meta http-equiv="expires" content="0" />
+    <meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+    <meta http-equiv="pragma" content="no-cache" />
+    <title>Pratik Restraurent</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="icon" href="images/fevicon/fevicon-pratik.png" type="image/x-icon">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+</head>
+<body>
+<header id="header" class="fixed-top">
+<nav class="navbar navbar-expand-lg navbar-dark p-lg-2 pt-0 pb-0">
+  <div class="container-fluid mobile-bar">
+    <a class="navbar-brand text-light font-weight-bold" href="#"><img class="m-sm-0 pratik-logo" src="images/logo/pratik new-logo.png" alt="pratik new-logo.png" class="responsive-img"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <i class="fas fa-bars text-white"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link text-light text-uppercase p-lg-4 active" aria-current="page" href="#link-home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light text-uppercase p-lg-4" href="#link-about"> About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light text-uppercase p-lg-4" href="#link-menu">Menu</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light text-uppercase p-lg-4" href="#link-contact">Contact Us</a>
+        </li>
+      </ul>      
+    </div>
+  </div>
+</nav>
+</header>
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner" id="link-home">  
+    <div class="carousel-item active" data-interval="10s">         
+      <img src="images/banner/rice.jpg" class="d-block w-100" alt="rice.jpg">
+    </div>
+    <div class="carousel-item" data-interval="20s">
+      <img src="images/banner/noodles.jpg" class="d-block w-100" alt="noodles.jpg">
+    </div>    
+  </div>  
+</div>
+<div class="container-fluid about-us bg-light"> 
+ <div class="row"> 
+  <div class="col-sm-1"></div>
+  <div class="col-sm-6 p-2" data-aos="fade-right">
+    <h1 class="about-h text-dark p-3" id="link-about">Our Story</h1>
+    <p class="text-dark about-p p-lg-3 p-sm-0 mt-4"> 
+    Pratik Restaurant was founded in 2018 by family. 	 	 <br><br>
+     We are serve Indian, Chinese & tandoor cuisine is a natural part of the diet in the markets in which the Pratik Restaurants operate. 
+     We continue to offer new menu options, both vegetarian and non-vegetarian, largely based on Indian , Chinese and tandoor cuisine to introduce new dishes. 
+     We also run popular dishes at our Pratik Restaurants, thereby offering our customers a range of Indian, Chinese and tandoor cuisines.<br><br>
+     Pratik Restaurants offers our customers a varying spread of consistently delicious food at a value-oriented price in a pleasant and casual dining environment.
+    </p>
+  </div>
+  <div class="col-sm-5 ">
+     <lottie-player src="https://assets4.lottiefiles.com/packages/lf20_kbgmw3ai/lottie_step5/data.json"  background="transparent"  speed="1"  style="max-width: 500px; max-height: 500px;"  loop  autoplay></lottie-player>
+  </div>
+ </div> 
+</div>
+<div id="link-menu" class="title text-light text-center p-lg-5 p-md-3">
+            <h4><span>Explore</span>Our Menu</h4>
+</div>
+<div class="container-fluid">
+ <div class="row">
+    <div class="card-menu col-sm-6" data-aos="flip-right">
+            <div class="single-menu">
+            <div class="img-center"><img src="images/menu/butter-chicken.jpg" alt="butter-chicken.jpg" width="150px"  height="150px"></div>
+                <div class="menu-content text-light p-2">
+                    <h4>Butter Chicken <span></span></h4>
+                    <p>Butter Chicken is a curry of chicken in a spiced tomato, butter and cream sauce. It originated in India as a curry.</p>
+                </div>
+            </div>
+    </div>
+    <div class="card-menu col-sm-6" data-aos="flip-right">
+            <div class="single-menu">
+                <div class="img-center"><img src="images/menu/chicken-biryani.jpg" alt="chicken-biryani.jpg"></div>
+                <div class="menu-content text-light p-2">
+                    <h4>Chicken Biryani<span></span></h4>
+                    <p>Chicken Biryani is a savory chicken and rice dish that includes layers of chicken, rice, and aromatics that are steamed together.</p>
+                </div>
+            </div>      
+    </div>
+ </div>  
+</div>
+<div class="container-fluid" >
+ <div class="row">
+    <div class="card-menu col-sm-6" data-aos="flip-right">
+            <div class="single-menu">
+                <div class="img-center"><img src="images/menu/Chicken-fried.jpg" alt="Chicken-fried.jpg"></div>
+                <div class="menu-content text-light p-2">
+                    <h4>Chicken Fried Rice<span></span></h4>
+                    <p>fried rice - boiled rice mixed with scallions and minced pork or shrimp and quickly scrambled with eggs.</p>
+                </div>
+            </div>
+    </div>
+    <div class="card-menu col-sm-6" data-aos="flip-right">
+            <div class="single-menu">
+                 <div class="img-center"><img src="images/menu/Chicken-Kabab.jpg" alt="Chicken-Kabab.jpg" width="150px"  height="150px"></div>
+                
+                <div class="menu-content text-light p-2">
+                    <h4>Chicken Tikka Kabab<span></span></h4>
+                    <p>Chicken Tikka Kebab are tender cooked chunks of chicken marinated in yogurt and spices that is cooked to perfection with mixed peppers and onions.</p>
+                </div>
+            </div>
+    </div>
+ </div>
+</div>
+<div class="container-fluid">   
+ <div class="row">
+    <div class="card-menu col-sm-6" data-aos="flip-right">
+            <div class="single-menu">
+                <div class="img-center"><img src="images/menu/Chicken-Tandoori.jpg"  class="" alt="Chicken-Tandoori.jpg"></div>
+                <div class="menu-content text-light p-2">
+                    <h4>Chicken Tandoori<span></span></h4>
+                    <p>Tandoori chicken is a chicken dish prepared by roasting chicken marinated in yogurt and spices in a tandoor.</p>
+                </div>
+            </div>
+    </div>
+    <div class="card-menu col-sm-6" data-aos="flip-right">
+            <div class="single-menu">
+                <div class="img-center"><img src="images/menu/Chicken-Masala.jpg" alt="Chicken-Masala.jpg"></div>
+                <div class="menu-content text-light p-2">
+                    <h4>Chicken Masala<span></span></h4>
+                    <p>Chicken masala is a dish consisting of roasted marinated chicken chunks in spiced curry sauce.</p>
+                </div>
+            </div>
+    </div>
+ </div>  
+ <div class="row">
+    <div class="card-menu col-sm-6" data-aos="flip-right">
+            <div class="single-menu">
+                <div class="img-center"><img src="images/menu/indian-bread.jpg"  class="" alt="indian-bread.jpg"></div>
+                <div class="menu-content text-light p-2">
+                    <h4>Indian Breads<span></span></h4>
+                    <p>Indian breads are a wide variety of flatbreads and crêpes which are an integral part of Indian cuisine.</p>
+                </div>
+            </div>
+    </div> 
+ </div>  
+</div>
+  <h1 class="text-center">
+      <a data-aos="zoom-in-up" href="images/menu-pdf/Pratik Menu.pdf" download><button type="button" class="btn btn-light text-uppercase">View all<i class="fa fa-angle-double-right p-2" aria-hidden="true"style='font-size:15px;'></i></button></a>
+  </h1> 
+<div class="hero-image">
+ <div class="hero-text">
+  <div class="container">
+   <div class="row"> 
+    <div class="col-sm-6 app-info">
+      <p class=" display-4 text-light ps-4 app-title">Order Online Food From Our App</p>     
+      <p class="text-light  about-p p-lg-2 p-sm-0 mt-4"> 
+      Find our application and download it, you can order food online and see your deliveries on the way and much more.     
+      </p>
+      <a class="mt-3 play"><img src="images/app/app2.png" width="200px" ></a>       
+    </div>
+    <div class="col-sm-6">
+    <img src="images/app/app-img.png" class="mobile_app_banner">
+    </div>
+   </div>
+  </div>    
+  </div>
+</div>
+<div class="container contact">
+  <div class="row">
+    <div class="col-sm-3">      
+    </div>
+    <div class="col-sm-6">      
+        <h1 id="link-contact" class="display-4 touch text-center text-light">Visit Us</h1>
+        <p class="address text-center text-light p-lg-3 p-sm-0">Building Number.3, 4, Main Road, Sangharsh Nagar, Chandivali, Andheri East, Mumbai, Maharashtra 400072</p>
+        <h1 class="text-center">
+            <button type="button" class="btn btn-light equal-btn">Email Us: example@gmail.com</button>  
+            <a href="tel: 8898242843"><button type="button" id="btn-mob" class="btn btn-light equal-btn">Call Us: 8898242843</button></a>           
+        </h1>
+        <h1 class="text-center social-icon">
+              <i class="fab fa-instagram" style='font-size:30px; color:white'></i>              
+              <i class="fab fa-facebook-f" style='font-size:30px; color:white; margin-left:20px'></i>
+              <i class="fab fa-twitter" style='font-size:30px; color:white; margin-left: 20px'></i>            
+              <i class="fab fa-whatsapp" style='font-size:30px; color:white; margin-left: 20px'></i>
+        </h1>
+        <p class="text-center text-light copy-right pt-5">Copyright © 2021 . All rights Reserved.<br> Designed by
+          METINFOS</p>
+    </div>    
+    <div class="col-sm-3">      
+    </div>
+  </div>
+</div>  
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+<script>
+// scroll
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 50)
+    {
+      $("#header").css("background","black");
+    }
+    else
+    {
+      $("#header").css("background","rgba(0,0,0,0.7)");
+    }
+  });
+});
+
+$(function(){ 
+     var navMain = $(".navbar-collapse");
+
+     navMain.on("click", "a", null, function () {
+         navMain.collapse('hide');
+     });
+ });
+ //Get the button
+
+</script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+  AOS.init();
+</script>
+</body>
+</html>
